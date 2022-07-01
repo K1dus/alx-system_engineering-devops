9b64eeb5444d5b12940e70a2e130993a79abb976
Kidus <kidus.efr@gmail.com>
2022-07-01 13:50:11 -0700

    adding 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,3 +2,4 @@ ng the number of failed requests to 0
 exec { 'fix--for-nginx':
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
